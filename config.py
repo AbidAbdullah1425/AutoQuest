21c51016dfc0f0078bf2741a3706eb6552ed4a7e
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-11-17 21:35:38 +0600

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,7 @@ DOWNLOAD_PATH = "./downloads/"
 # Polling Interval for RSS Check
 CHECK_INTERVAL = 600  # 10 minutes
 
+PORT = os.environ.get("PORT", "8080")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
